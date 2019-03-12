12a0f1c3959b221300b03d3b29c01190bc59fc85
testwebnode <32686799+testwebnode@users.noreply.github.com>
2019-03-12 23:23:22 +0100

    Update getmatch.py

--- getmatch.py
+++ getmatch.py
@@ -10,6 +10,13 @@ with open(file2, 'r') as f2:
   str_list2 = [line.strip() for line in f2]
 
 best_match=[]
-best_match.append(difflib.get_close_matches(str_list1[0],str_list2,1)[0])
-#score = difflib.SequenceMatcher(None, my_str, best_match).ratio()
-print best_match
+#print str_list1
+for i in range(len(str_list1)):
+  #print i
+  #print str_list1[i]
+  bm=difflib.get_close_matches(str_list1[i],str_list2,1)
+  if bm: 
+    best_match.append(bm[0])
+  else:
+    best_match.append("")
+  print best_match[i]
