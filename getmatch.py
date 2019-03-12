de507d9d65179a9d2dce28c7e29574f6c8a71059
testwebnode <32686799+testwebnode@users.noreply.github.com>
2019-03-12 22:46:51 +0100

    Update getmatch.py

--- getmatch.py
+++ getmatch.py
@@ -9,6 +9,6 @@ with open(file1, 'r') as f1:
 with open(file2, 'r') as f2:    
   str_list2 = [line.strip() for line in f2]
 
-best_match = difflib.get_close_matches(str_list1[0],str_list,1)[0]
+best_match = difflib.get_close_matches(str_list1[0],str_list2,1)[0]
 #score = difflib.SequenceMatcher(None, my_str, best_match).ratio()
 print best_match
