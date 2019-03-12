f78a77e04f3a82f221ce71e3c3a2e34ae7766f7c
testwebnode <32686799+testwebnode@users.noreply.github.com>
2019-03-12 22:34:05 +0100

    update two files as input

--- getmatch.py
+++ getmatch.py
@@ -1,8 +1,14 @@
 import difflib
 import sys
-my_str = sys.argv[1]
-file= sys.argv[2]
-with open(file, 'r') as f:    
-  str_list = [line.strip() for line in f]
-best_match = difflib.get_close_matches(my_str,str_list,1)[0]
-score = difflib.SequenceMatcher(None, my_str, best_match).ratio()
+file1= sys.argv[1]
+file2= sys.argv[2]
+
+with open(file1, 'r') as f1:    
+  str_list1 = [line.strip() for line in f1]
+
+with open(file2, 'r') as f2:    
+  str_list2 = [line.strip() for line in f2]
+
+best_match = difflib.get_close_matches(str_list1[0],str_list,1)[0]
+#score = difflib.SequenceMatcher(None, my_str, best_match).ratio()
+return best_match
